0b12a15e1bc1b5425c370554d566d6b19c6eb5f5
Odedele Emmanuel <odedeleemmanuel@gmail.com>
2022-03-11 12:22:29 -0800

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
-int main(void)
+int main(void))
 {
 	int n;
 
@@ -17,16 +17,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	  /* your code goes there */
 	if (n > 0)
-       	{
-			printf("%i is positive\n", n);
+	{
+		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
 	{
-			printf("%i is zero\n", n);
+		printf("%i is zero\n", n);
 	}
 	else if (n < 0)
 	{
-			printf("%i is negative\n", n);
+		printf("%i is negative\n", n);
 	}
 	return (0);
 }
